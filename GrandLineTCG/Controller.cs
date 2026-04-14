3c48a62c979428dafc13df87027094216df74c79
fillip husebø <felipehusebo@gmail.com>
2026-04-14 16:14:12 +0200

    added login function

--- GrandLineTCG/Controller.cs
+++ GrandLineTCG/Controller.cs
@@ -13,4 +13,14 @@ public class Controller
         _users.Add(user);
         return user;
     }
+    
+    public User Login(string username, string password)
+    {
+        var user = _users.FirstOrDefault(u => u.Username == username);
+
+        if (user == null || !user.CheckPassword(password))
+            throw new InvalidOperationException("invalid username or password");
+        
+        return user;
+    }
 }
