ceb5fc8fbe7ce19f030040d7ddce48b76c07d270
fillip husebø <felipehusebo@gmail.com>
2026-04-14 13:53:45 +0200

    created user class

--- GrandLineTCG/User.cs
+++ GrandLineTCG/User.cs
@@ -2,5 +2,30 @@ namespace GrandLineTCG;
 
 public class User
 {
+    public string Username { get; set; }
     
+    private string Password { get; set; }
+    
+    public DateTime AccountCreated { get; set; } = DateTime.Now;
+
+    public List<Events> Host { get; set; } = new();
+   
+    public List<Events> Attending { get; set; } = new();
+
+    public List<Booking> Purchases { get; set; } = new();
+    
+    public List<Booking> Sales { get; set; } = new();
+
+    public List<Review> ReviewsReceived { get; set; } = new();
+    
+    public User (string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+    
+    public bool CheckPassword(string password)
+    {
+        return password == Password;
+    }
 }
