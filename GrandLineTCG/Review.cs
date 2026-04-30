ad09f23d671ce1a8ed062d6e37b86c3b70f7225e
fillip husebø <felipehusebo@gmail.com>
2026-04-30 17:44:25 +0200

    fixed profile class to fit

--- GrandLineTCG/Review.cs
+++ GrandLineTCG/Review.cs
@@ -4,7 +4,7 @@ public class Review
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public User Author { get; set; }
-    public Tournament Tournament { get; set; }
+    public BaseEvent Event { get; set; }
     public int Rating { get; set; }
     public string? Comment { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
