5618ab994b595e2a133b6da7b0532b527e4f6824
Berken Ates <berken_93@hotmail.com>
2026-05-01 09:14:49 +0200

    Updated Booking to include TicketTYpe and release slot on cancel

--- GrandLineTCG/Booking.cs
+++ GrandLineTCG/Booking.cs
@@ -7,16 +7,18 @@ public class Booking
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Reference => $"BK-{Id.ToString()[..5].ToUpper()}";
     public User Participant { get; set; }
-    public IEvent Event { get; set; } 
+    public IEvent Event { get; set; }
+    public TicketType TicketType { get; set; }
     public decimal PriceAtBooking { get; set; }
     public DateTime BookedAt { get; set; } = DateTime.Now;
     public BookingStatus Status { get; set; }
 
-    public Booking(User participant, IEvent @event, decimal priceAtBooking)
+    public Booking(User participant, IEvent @event, TicketType ticketType)
     {
         Participant = participant;
         Event = @event;
-        PriceAtBooking = priceAtBooking;
+        TicketType = ticketType;
+        PriceAtBooking = ticketType.Price;
         Status = BookingStatus.Confirmed;
     }
 
@@ -25,5 +27,6 @@ public class Booking
         if (Status == BookingStatus.Cancelled)
             throw new InvalidOperationException("This booking is already cancelled.");
         Status = BookingStatus.Cancelled;
+        TicketType.Release();
     }
 }
