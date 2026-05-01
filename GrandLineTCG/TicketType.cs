5a96b497a5887031c9d6c797abd77bc3326fa80e
Berken Ates <berken_93@hotmail.com>
2026-05-01 08:58:41 +0200

    Created TicketType class with Book and Release methods

---
+++ GrandLineTCG/TicketType.cs
@@ -0,0 +1,33 @@
+﻿namespace GrandLineTCG;
+
+public class TicketType
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity  { get; set; }
+    public int Remaining { get; set; }
+    
+    public bool IsAvailable => Remaining > 0;
+
+    public TicketType(string name, decimal price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+        Remaining = quantity;
+    }
+
+    public bool Book()
+    {
+        if (!IsAvailable) return  false;
+        Remaining--;
+        return true;
+    }
+
+    public void Release()
+    {
+        if (Remaining < Quantity)
+            Remaining++;
+    }
+}
